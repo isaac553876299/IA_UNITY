ff5fca70d52f29129de8958682ff0e29c5d0b6c0
isaac553876299 <isaacdd.bcn.2001@gmail.com>
2021-11-14 01:42:57 +0100

    almost finished

--- AI Planner/Assets/Scripts/PlannerCallbacks.cs
+++ AI Planner/Assets/Scripts/PlannerCallbacks.cs
@@ -1,33 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Generated.Semantic.Traits;
 
 public class PlannerCallbacks : MonoBehaviour
 {
-    Moves moves;
-    UnityEngine.AI.NavMeshAgent agent;
-
-    void Start()
-    {
-        moves = this.GetComponent<Moves>();
-        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
-    }
+    public Moves moves;
+    public UnityEngine.AI.NavMeshAgent agent;
+    public Robber trt;
 
     public void Steal(GameObject treasure)
     {
+        Debug.Log("Steal");
         treasure.GetComponent<Renderer>().enabled = false;
     }
 
     public IEnumerator Seek(GameObject treasure, GameObject copGO)
     {
+        Debug.Log("Approach");
         agent.SetDestination(treasure.transform.position);
         while ((Vector3.Distance(treasure.transform.position, transform.position) > 2f) &&
                (Vector3.Distance(treasure.transform.position, copGO.transform.position) > 10f))
             yield return null;
+        if (Vector3.Distance(treasure.transform.position, copGO.transform.position) < 10f)
+        {
+            trt.CopAway = false;
+        }
+        else
+        {
+            trt.Ready2Steal = true;
+        }
     }
 
     public IEnumerator Wander(GameObject cop, GameObject treasure)
     {
+        Debug.Log("Wander");
         while (Vector3.Distance(treasure.transform.position, cop.transform.position) < 10f)
         {
             moves.Wander();
