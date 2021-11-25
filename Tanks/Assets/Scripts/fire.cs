fdfa69512ddf71ec3ee0a440241a6fd633d4b70a
isaacdd <isaacdd.bcn.2001@gmail.com>
2021-11-25 22:37:40 +0100

    h

--- Tanks/Assets/Scripts/fire.cs
+++ Tanks/Assets/Scripts/fire.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class fire : MonoBehaviour
 {
+    public Transform target;
     public int m_PlayerNumber = 1;              // Used to identify the different players.
     public Rigidbody m_Shell;                   // Prefab of the shell.
     public Transform m_FireTransform;           // A child of the tank where the shells are spawned.
     public AudioSource m_ShootingAudio;         // Reference to the audio source used to play the shooting audio. NB: different to the movement audio source.
     public AudioClip m_ChargingClip;            // Audio that plays when each shot is charging up.
     public AudioClip m_FireClip;                // Audio that plays when each shot is fired.
-    public float m_LaunchForce = 20f;
+    public float m_LaunchForce = 30f;
+    public int shells = 3;
 
-    private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
+    private bool m_Fired = false;                       // Whether or not the shell has been launched with this button press.
 
     private void Start()
     {
@@ -20,28 +23,33 @@ public class fire : MonoBehaviour
 
     private void Update()
     {
-        Fire();
+        if(!m_Fired) Fire();
     }
 
     private void Fire()
     {
-        const float g = 9.8f;
-        float v = m_LaunchForce;
-        //float angle = atan(v * v + -sqrt(v * v * v * v - g * (g * x * x + 2 * y * v * v))) / (g * x);
+        float maxDist = m_LaunchForce * Mathf.Cos(0.79f);
 
-        // Set the fired flag so only Fire is only called once.
-        m_Fired = true;
+        if (maxDist > Vector3.Distance(m_FireTransform.position, target.position))
+        {
+            const float g = 10f;
+            float v = m_LaunchForce;
+            float x = target.position.x;
+            float y = target.position.y;
+            float angle = Mathf.Atan(Mathf.Pow(v, 2) + Mathf.Sqrt(Mathf.Pow(v, 4) - g * (g * Mathf.Pow(x, 2) + 2 * y * Mathf.Pow(v, 2))) / (g * x));
+            Debug.Log(angle);
+            m_Fired = true;
 
-        // Create an instance of the shell and store a reference to it's rigidbody.
-        Rigidbody shellInstance =
-            Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+            Rigidbody shellInstance =
+                Instantiate(m_Shell, m_FireTransform.position,
+                m_FireTransform.rotation) as Rigidbody;
 
-        // Set the shell's velocity to the launch force in the fire position's forward direction.
-        shellInstance.velocity = m_LaunchForce * m_FireTransform.forward;
+            shellInstance.velocity = m_LaunchForce * m_FireTransform.forward;
 
-        // Change the clip to the firing clip and play it.
-        m_ShootingAudio.clip = m_FireClip;
-        m_ShootingAudio.Play();
+            m_ShootingAudio.clip = m_FireClip;
+            m_ShootingAudio.Play();
 
+            --shells;
+        }
     }
 }
