d1d99d87bdf4f652c28f246a26bbd8945511b066
Ruizo <marc_ruiz1995@hotmail.com>
2021-11-27 02:23:27 +0100

    patrol behaviour

--- Tanks/Assets/Scripts/patrol.cs
+++ Tanks/Assets/Scripts/patrol.cs
@@ -1,36 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
+using Pada1.BBCore.Tasks;
+using Pada1.BBCore;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class patrol : MonoBehaviour
+namespace BBUnity.Actions
 {
-    private NavMeshAgent agent;
-    private Transform target;
-
-    public GameObject[] waypoints;
-    int patrolWP = -1;
-
-    // Start is called before the first frame update
-    void Start()
+    [Action("Actions/Patrol")]
+    public class Patrol : GOAction
     {
-        agent = gameObject.GetComponent<NavMeshAgent>();
-        target = GameObject.FindGameObjectWithTag("blueTank").transform;
-    }
+        private UnityEngine.AI.NavMeshAgent navAgent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Patrol();
-    }
+        [InParam("wp1")]
+        public Vector3 wp1;
+        [InParam("wp2")]
+        public Vector3 wp2;
+        [InParam("wp3")]
+        public Vector3 wp3;
+        [InParam("wp4")]
+        public Vector3 wp4;
+        [InParam("wp5")]
+        public Vector3 wp5;
 
-    void Patrol()
-    {
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        public Vector3[] waypoints;
+        int patrolWP = -1;
+
+        public override void OnStart()
         {
-            patrolWP = (patrolWP + 1) % waypoints.Length;
-            agent.destination=(waypoints[patrolWP].transform.position);
+            navAgent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+            if (navAgent == null)
+            {
+                Debug.LogWarning("The " + gameObject.name + " game object does not have a Nav Mesh Agent component to navigate. One with default values has been added", gameObject);
+                navAgent = gameObject.AddComponent<UnityEngine.AI.NavMeshAgent>();
+            }
+
+            waypoints = new Vector3[5];
+            waypoints[0] = wp1;
+            waypoints[1] = wp2;
+            waypoints[2] = wp3;
+            waypoints[3] = wp4;
+            waypoints[4] = wp5;
+            
+
+#if UNITY_5_6_OR_NEWER
+                navAgent.isStopped = false;
+#else
+            navAgent.Resume();
+#endif
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            if (!navAgent.pathPending && navAgent.remainingDistance < 0.5f)
+            {
+                patrolWP = (patrolWP + 1) % waypoints.Length;
+                navAgent.destination = (waypoints[patrolWP]);
+            }
+
+            if (!navAgent.pathPending && navAgent.remainingDistance <= navAgent.stoppingDistance)
+                return TaskStatus.COMPLETED;
+
+            return TaskStatus.RUNNING;
+        }
+
+        public override void OnAbort()
+        {
+#if UNITY_5_6_OR_NEWER
+            if(navAgent!=null)
+                navAgent.isStopped = true;
+#else
+            if (navAgent != null)
+                navAgent.Stop();
+#endif
         }
     }
-
 }
