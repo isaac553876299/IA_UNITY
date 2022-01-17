cb26f25c99f1694c995f8f076cc9f0e2f1ecfcaa
isaacdd <isaacdd.bcn.2001@gmail.com>
2022-01-17 20:18:22 +0100

    some wokr done

--- mlagents/Assets/Scripts/RollerAgent.cs
+++ mlagents/Assets/Scripts/RollerAgent.cs
@@ -7,12 +7,18 @@ using Unity.MLAgents.Actuators;
 
 public class RollerAgent : Agent
 {
-    Rigidbody rigidBody;
-    public Transform target;
-    public float forceMultiplier = 10;
-
+    public List<GameObject> disableTriggers = new List<GameObject>();
     public Transform child;
-    public float wallMinDistance;
+    private Vector3 newPos;
+    [SerializeField] public float time1;
+    [SerializeField] private float time2;
+
+    public Transform[] possibleTargetSpawns;
+    Rigidbody rBody;
+    Vector3 targetStartPos;
+    Vector3 playerStartPos;
+    public float wallMinDistance = 1f;
+    [SerializeField] private bool isWall = false;
     [SerializeField] private float fp;
     [SerializeField] private float fm;
     [SerializeField] private float rp;
@@ -20,107 +26,123 @@ public class RollerAgent : Agent
 
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();
+        rBody = GetComponent<Rigidbody>();
+        targetStartPos = target.transform.position;
+        playerStartPos = transform.position;
+        time2 = time1;
     }
+
+    public Transform target;
 
     public override void OnEpisodeBegin()
     {
-        this.transform.localPosition = new Vector3(10, 0.5f,-10);
-        this.rigidBody.velocity = Vector3.zero;
-        this.rigidBody.angularVelocity = Vector3.zero;
+        time1 = time2;
+        // If the Agent fell, zero its momentum
+        if (this.transform.localPosition.y < 0)
+        {
+            this.rBody.angularVelocity = Vector3.zero;
+            this.rBody.velocity = Vector3.zero;
+            this.transform.localPosition = new Vector3(0, 0.5f, 0);
+        }
+        for (int i = 0; i < disableTriggers.Count; i++)
+        {
+            disableTriggers[i].GetComponent<MeshRenderer>().enabled = true;
+            disableTriggers[i].GetComponent<BoxCollider>().enabled = true;
+            disableTriggers[i].GetComponent<BoxCollider>().isTrigger = true;
+            disableTriggers.RemoveAt(i);
+        }
 
-        target.transform.localPosition = new Vector3(
-            Random.value * 2 + 10,
-            0.5f,
-            Random.value * 2 - 10);
+        transform.position = playerStartPos;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
+        // Target and Agent positions
         sensor.AddObservation(target.localPosition);
         sensor.AddObservation(this.transform.localPosition);
-
-        sensor.AddObservation(rigidBody.velocity.x);
-        sensor.AddObservation(rigidBody.velocity.z);
+        // Agent velocity
+        sensor.AddObservation(rBody.velocity.x);
+        sensor.AddObservation(rBody.velocity.z);
+        sensor.AddObservation(isWall);
+        if (isWall)
+            isWall = false;
+        //RAYCAST INFO
+        //sensor.AddObservation(fp);
+        //sensor.AddObservation(rp);
+        //sensor.AddObservation(fm);
+        //sensor.AddObservation(rm);
     }
+
+    public float forceMultiplier = 10;
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
+        // Actions, size = 2
         Vector3 controlSignal = Vector3.zero;
         controlSignal.x = actionBuffers.ContinuousActions[0];
         controlSignal.z = actionBuffers.ContinuousActions[1];
-        rigidBody.AddForce(controlSignal * forceMultiplier);
-
-        if (this.transform.localPosition.y < 0)
+        rBody.AddForce(controlSignal * forceMultiplier);
+        
+        if (time1 >= 0)
         {
-            // fell off
+            time1 -= Time.deltaTime;
+        }
+        else
+        {
+            EndEpisode();
+            time1 = time2;
+        }
+        // Rewards
+        float distanceToTarget = Vector3.Distance(this.transform.localPosition, target.localPosition);
+        // Reached target
+        if (distanceToTarget < 1.42f)
+        {
+            SetReward(1.0f);
+            //target.localPosition = possibleTargetSpawns[Random.RandomRange(0, possibleTargetSpawns.Length)].position;
+
             EndEpisode();
         }
-
-        RaycastHit hitForward;
-        RaycastHit hitRight;
-        RaycastHit hitMinusForward;
-        RaycastHit hitMinusRight;
-        // RAYCAST FORWARD +
-        if (Physics.Raycast(child.transform.position, child.transform.forward, out hitForward, wallMinDistance))
+        // Fell off platform
+        else if (this.transform.localPosition.y < 0)
         {
-            if (hitForward.transform.tag == "wall")
-            {
-                fp = hitForward.distance;
-            }
-            Debug.DrawLine(child.transform.position, hitForward.transform.position, Color.black);
-        }
-        // RAYCAST RIGHT +
-        if (Physics.Raycast(child.transform.position, child.transform.right, out hitRight, wallMinDistance))
-        {
-            if (hitRight.transform.tag == "wall")
-            {
-                rp = hitRight.distance;
-            }
-            Debug.DrawLine(child.transform.position, hitRight.transform.position, Color.black);
-        }
-        // RAYCAST FORWARD -
-        if (Physics.Raycast(child.transform.position, -child.transform.forward, out hitMinusForward, wallMinDistance))
-        {
-            if (hitMinusForward.transform.tag == "wall")
-            {
-                fm = hitMinusForward.distance;
-            }
-            Debug.DrawLine(child.transform.position, hitMinusForward.transform.position, Color.black);
-        }
-        // RAYCAST RIGHT -
-        if (Physics.Raycast(child.transform.position, -child.transform.right, out hitMinusRight, wallMinDistance))
-        {
-            if (hitMinusRight.transform.tag == "wall")
-            {
-                rm = hitMinusRight.distance;
-            }
-            Debug.DrawLine(child.transform.position, hitMinusRight.transform.position, Color.black);
+            EndEpisode();
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "target")
+        if (collision.transform.tag == "Wall")
         {
+            isWall = true;
+        }
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.tag == "Wall")
+        {
+            isWall = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "positiveTr")
+        {
+            disableTriggers.Add(other.gameObject);
+            other.GetComponent<BoxCollider>().enabled = false;
+            other.GetComponent<MeshRenderer>().enabled = false;
             SetReward(1.0f);
-            EndEpisode();
         }
-        if (collision.gameObject.tag == "wall")
+        if (other.tag == "negativeTr")
         {
+            disableTriggers.Add(other.gameObject);
+            other.GetComponent<BoxCollider>().enabled = false;
+            other.GetComponent<MeshRenderer>().enabled = false;
+            SetReward(-1.0f);
+        }
+        if (other.tag == "sp")
+        {
+            SetReward(-1.0f);
             EndEpisode();
         }
-    }
-
-    public override void Heuristic(in ActionBuffers actionsOut)
-    {
-        var continuousActionsOut = actionsOut.ContinuousActions;
-        continuousActionsOut[0] = Input.GetAxis("Horizontal");
-        continuousActionsOut[1] = Input.GetAxis("Vertical");
-    }
-
-    void Update()
-    {
-        
     }
 }
