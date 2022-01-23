9a51bb22a938ce15fddf94b7ab570dbfd70a76e9
isaacdd <isaacdd.bcn.2001@gmail.com>
2022-01-23 23:32:23 +0100

    final

--- mlagents/Assets/Scripts/RollerAgent.cs
+++ mlagents/Assets/Scripts/RollerAgent.cs
@@ -7,23 +7,17 @@ using Unity.MLAgents.Actuators;
 
 public class RollerAgent : Agent
 {
-    public List<GameObject> disableTriggers = new List<GameObject>();
-    public Transform child;
-    private Vector3 newPos;
     [SerializeField] public float time1;
     [SerializeField] private float time2;
+    public List<GameObject> disableTriggers = new List<GameObject>();
 
     public Transform[] possibleTargetSpawns;
     Rigidbody rBody;
-    Vector3 targetStartPos;
-    Vector3 playerStartPos;
     [SerializeField] private bool isWall = false;
 
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
-        targetStartPos = target.transform.position;
-        playerStartPos = transform.position;
         time2 = time1;
     }
 
@@ -32,13 +26,6 @@ public class RollerAgent : Agent
     public override void OnEpisodeBegin()
     {
         time1 = time2;
-        // If the Agent fell, zero its momentum
-        if (this.transform.localPosition.y < 0)
-        {
-            this.rBody.angularVelocity = Vector3.zero;
-            this.rBody.velocity = Vector3.zero;
-            this.transform.localPosition = new Vector3(0, 0.5f, 0);
-        }
         for (int i = 0; i < disableTriggers.Count; i++)
         {
             disableTriggers[i].GetComponent<MeshRenderer>().enabled = true;
@@ -62,71 +49,49 @@ public class RollerAgent : Agent
 
     public float forceMultiplier = 10;
 
-    public override void OnActionReceived(ActionBuffers actionBuffers)
+    public override void OnActionReceived(ActionBuffers actions)
     {
-        Vector3 controlSignal = Vector3.zero;
-        controlSignal.x = actionBuffers.ContinuousActions[0];
-        controlSignal.z = actionBuffers.ContinuousActions[1];
-        rBody.AddForce(controlSignal * forceMultiplier);
+		Vector3 control =
+			new Vector3(actions.ContinuousActions[0], 0, actions.ContinuousActions[1]);
+        rBody.AddForce(control * forceMultiplier);
         
-        if (time1 >= 0)
-        {
-            time1 -= Time.deltaTime;
-        }
+        if (time1 >= 0) time1 -= Time.deltaTime;
         else
         {
             EndEpisode();
             time1 = time2;
         }
-        // Rewards
-        float distanceToTarget = Vector3.Distance(this.transform.localPosition, target.localPosition);
-        // Reached target
-        if (distanceToTarget < 1.42f)
+
+        if (Vector3.Distance(this.transform.localPosition, target.localPosition) < 1.5f)
         {
             SetReward(1.0f);
-            EndEpisode();
-        }
-        // Fell off platform
-        else if (this.transform.localPosition.y < 0)
-        {
             EndEpisode();
         }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == "wall")
-        {
-            isWall = true;
-        }
-    }
-    private void OnCollisionStay(Collision collision)
+		isWall = (collision.transform.tag == "wall");
+	}
+	private void OnCollisionStay(Collision collision)
     {
-        if (collision.transform.tag == "wall")
-        {
-            isWall = true;
-        }
-    }
+		isWall = (collision.transform.tag == "wall");
+	}
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "positiveTr")
         {
+            SetReward(0.3f);
             disableTriggers.Add(other.gameObject);
             other.GetComponent<BoxCollider>().enabled = false;
             other.GetComponent<MeshRenderer>().enabled = false;
-            SetReward(1.0f);
         }
         if (other.tag == "negativeTr")
         {
+            SetReward(-0.5f);
             disableTriggers.Add(other.gameObject);
             other.GetComponent<BoxCollider>().enabled = false;
             other.GetComponent<MeshRenderer>().enabled = false;
-            SetReward(-1.0f);
-        }
-        if (other.tag == "sp")
-        {
-            SetReward(-1.0f);
-            EndEpisode();
         }
     }
 }
